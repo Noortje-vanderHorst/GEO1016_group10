473277994b416843ad10389da3fd353b1b2fc147
Noortje van der Horst <n.vanderhorst@student.tudelft.nl>
2021-02-14 16:40:36 +0100

    added c++ code (and other files) originally provided

---
+++ Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/easy3d/fileio/image_io.cpp" "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/image_io.cpp.obj"
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/easy3d/fileio/resources.cpp" "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/resources.cpp.obj"
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/easy3d/fileio/surface_mesh_io.cpp" "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/surface_mesh_io.cpp.obj"
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/easy3d/fileio/surface_mesh_io_obj.cpp" "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/fileio/CMakeFiles/easy3d_fileio.dir/surface_mesh_io_obj.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "EASY3D_RESOURCES_DIR=\"C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/resources\""
+  "_CRT_SECURE_NO_DEPRECATE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "."
+  "../3rd_party/LAStools/LASzip/src"
+  "../3rd_party/LAStools/LASlib/inc"
+  "../3rd_party/glog/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/core/CMakeFiles/easy3d_core.dir/DependInfo.cmake"
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/easy3d/util/CMakeFiles/easy3d_util.dir/DependInfo.cmake"
+  "C:/Users/noort/Documents/1/TU/year_4/GEO1016_3D_computer_vision/Assignments/GEO1016_group10/Assignment1/A1_Calibration/A1_Calibration_Code/cmake-build-debug/3rd_party/glog/CMakeFiles/3rd_glog.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
