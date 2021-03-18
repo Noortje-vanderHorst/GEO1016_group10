8dadb01a1e120a80325c38ce17971b30bd1589a7
jasper van der vaart <jasper.vaart@live.nl>
2021-03-18 15:16:46 +0100

    Added validity check to input points and removed examples

--- Assignment2/A2_Triangulation/A2_Triangulation_Code/Triangulation/triangulation_method.cpp
+++ Assignment2/A2_Triangulation/A2_Triangulation_Code/Triangulation/triangulation_method.cpp
@@ -57,115 +57,65 @@ Matrix<double> to_Matrix(const mat &M) {
     return result;
 }
 
-
-/// comments and hints provided (we can just throw this whole thing away in the final version)
-void Comments(){
-    /// NOTE: there might be multiple workflows for reconstructing 3D geometry from corresponding image points.
-    ///       This assignment uses the commonly used one explained in our lecture.
-    ///       It is advised to define a function for each sub-task. This way you have a clean and well-structured
-    ///       implementation, which also makes testing and debugging easier. You can put your other functions above
-    ///       triangulation(), or feel free to put them in one or multiple separate files.
-
-    std::cout << "\nTODO: I am going to implement the triangulation() function in the following file:" << std::endl
-              << "\t    - triangulation_method.cpp\n\n";
-
-    std::cout << "[Liangliang]:\n"
-                 "\tFeel free to use any data structure and function offered by Easy3D, in particular the following two\n"
-                 "\tfiles for vectors and matrices:\n"
-                 "\t    - easy3d/core/mat.h  Fixed-size matrices and related functions.\n"
-                 "\t    - easy3d/core/vec.h  Fixed-size vectors and related functions.\n"
-                 "\tFor matrices with unknown sizes (e.g., when handling an unknown number of corresponding points\n"
-                 "\tstored in a file, where their sizes can only be known at run time), a dynamic-sized matrix data\n"
-                 "\tstructure is necessary. In this case, you can use the templated 'Matrix' class defined in\n"
-                 "\t    - Triangulation/matrix.h  Matrices of arbitrary dimensions and related functions.\n"
-                 "\tPlease refer to the corresponding header files for more details of these data structures.\n\n"
-                 "\tIf you choose to implement the non-linear method for triangulation (optional task). Please refer to\n"
-                 "\t'Tutorial_NonlinearLeastSquares/main.cpp' for an example and some explanations. \n\n"
-                 "\tIn your final submission, please\n"
-                 "\t    - delete ALL unrelated test or debug code and avoid unnecessary output.\n"
-                 "\t    - include all the source code (original code framework + your implementation).\n"
-                 "\t    - do NOT include the 'build' directory (which contains the intermediate files in a build step).\n"
-                 "\t    - make sure your code compiles and can reproduce your results without any modification.\n\n" << std::flush;
-
-    /// Easy3D provides fixed-size matrix types, e.g., mat2 (2x2), mat3 (3x3), mat4 (4x4), mat34 (3x4).
-    /// To use these matrices, their sizes should be known to you at the compile-time (i.e., when compiling your code).
-    /// Once defined, their sizes can NOT be changed.
-    /// In 'Triangulation/matrix.h', another templated 'Matrix' type is also provided. This type can have arbitrary
-    /// dimensions and their sizes can be specified at run-time (i.e., when executing your program).
-    /// Below are a few examples showing some of these data structures and related APIs.
-
-    /// ----------- fixed-size matrices
-
-    /// define a 3 by 4 matrix M (you can also define 3 by 4 matrix similarly)
-    mat34 M(1.0f);  /// entries on the diagonal are initialized to be 1 and others to be 0.
-
-    /// set the first row of M
-    M.set_row(0, vec4(1,1,1,1));    /// vec4 is a 4D vector.
-
-    /// set the second column of M
-    M.set_col(1, vec4(2,2,2,2));
-
-    /// get the 3 rows of M
-    vec4 M1 = M.row(0);
-    vec4 M2 = M.row(1);
-    vec4 M3 = M.row(2);
-
-    /// ----------- fixed-size vectors
-
-    /// how to quickly initialize a std::vector
-    std::vector<double> rows = {0, 1, 2, 3,
-                                4, 5, 6, 7,
-                                8, 9, 10, 11};
-    /// get the '2'-th row of M
-    const vec4 b = M.row(2);    // it assigns the requested row to a new vector b
-
-    /// get the '1'-th column of M
-    const vec3 c = M.col(1);    // it assigns the requested column to a new vector c
-
-    /// modify the element value at row 2 and column 1 (Note the 0-based indices)
-    M(2, 1) = b.x;
-
-    /// apply transformation M on a 3D point p (p is a 3D vector)
-    vec3 p(222, 444, 333);
-    vec3 proj = M * vec4(p, 1.0f);  // use the homogenous coordinates. result is a 3D vector
-
-    /// the length of a vector
-    float len = p.length();
-    /// the squared length of a vector
-    float sqr_len = p.length2();
-
-    /// the dot product of two vectors
-    float dot_prod = dot(p, proj);
-
-    /// the cross product of two vectors
-    vec3 cross_prod = cross(p, proj);
-
-    /// normalize this vector
-    cross_prod.normalize();
-
-    /// a 3 by 3 matrix (all entries are intentionally NOT initialized for efficiency reasons)
-    mat3 F;
-    /// ... here you compute or initialize F.
-    /// compute the inverse of K
-    mat3 invF = inverse(F);
-
-    /// ----------- dynamic-size matrices
-
-    /// define a non-fixed size matrix
-    Matrix<double> W(2, 3, 0.0); // all entries initialized to 0.0.
-
-    /// set its first row by a 3D vector (1.1, 2.2, 3.3)
-    W.set_row({ 1.1, 2.2, 3.3 }, 0);   // here "{ 1.1, 2.2, 3.3 }" is of type 'std::vector<double>'
-
-    /// get the last column of a matrix
-    std::vector<double> last_column = W.get_column(W.cols() - 1);
-
-    // TODO: delete all above demo code in the final submission
-}
-
-
 /// check input validity
-void test_input(){};
+void test_input(const std::vector<vec3> &points_1, const std::vector<vec3> &points_2){
+
+    std::vector<int> duplicateLocations;
+
+    if (points_1.size() != points_2.size()){
+        unsigned int mis = points_2.size() - points_1.size();
+        std::cout << "point pairs do not match!" << std::endl;
+        std::cout << "mismatch of:" << mis << " points!" << std::endl;
+        return;
+    }
+
+    for (unsigned int i = 0; i < points_1.size(); i ++ ) {
+        // check for negative 3D points
+        if (points_1[i].x < 0 || points_1[i].y < 0  ||
+            points_2[i].x < 0 || points_2[i].y < 0 ) {
+            std::cout << "Invalid 2d point with negative coordinates: ("
+                      << points_1[i].x << " "
+                      << points_1[i].z << ") at location: "
+                      << i << std::endl;
+
+            duplicateLocations.emplace_back(i);
+            continue;
+        }
+
+        if (points_1[i].z != 1 || points_2[i].z != 1){
+            std::cout << "Invalid constructed 2d point: ("
+                      << points_1[i].x << " "
+                      << points_1[i].z << ") at location: "
+                      << i << std::endl;
+
+            duplicateLocations.emplace_back(i);
+            continue;
+        }
+
+        // check for duplicates
+        for (unsigned int j = 0; j < points_1.size(); j ++) {
+            if ( i >= j) {
+                continue;
+            }
+            if (points_1[i].x == points_1[j].x
+                && points_1[i].y == points_1[j].y) {
+
+                std::cout << "Duplicate 2d point with coordinates: ("
+                          << points_1[i][0] << " "
+                          << points_1[i][1] << ") at location: "
+                          << i << std::endl;
+
+                duplicateLocations.emplace_back(i);
+                break;
+            }
+        }
+    }
+
+    // check if the size after removal is big enough
+    if (points_1.size() - duplicateLocations.size() < 8){
+        std::cerr << "expecting at least 8 unique pairs of corresponding points" << std::endl;
+    }
+}
 
 
 /// Normalize input points
@@ -546,7 +496,7 @@ bool Triangulation::triangulation(
     // implementation starts ...
 
     // TODO: check if the input is valid (always good because you never known how others will call your function).
-    test_input();
+    test_input(points_0, points_1);
 
     // TODO: Estimate relative pose of two views. This can be subdivided into:
 
